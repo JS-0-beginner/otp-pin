5f3726eee089d55caa09b90b6c09c0efc8897031
avishekataur <avishekataur@gmail.com>
2021-08-18 23:10:04 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,66 @@
+function getPin()
+{
+    const pin = Math.round(Math.random() * 10000);
+    const pinString = pin + '';
+    if(pinString.length == 4)
+    {
+        return pin;
+    }
+    else
+    {
+        return getPin()
+    }
+}
+
+function generatePin()
+{
+    const pin = getPin();
+    document.getElementById('display-pin').value = pin;
+    
+}
+
+document.getElementById('keypad').addEventListener('click',
+
+function(event)
+{
+    const number = (event.target.innerText);
+    const calcInput = document.getElementById('typed-numbers');
+
+    if(isNaN(number))
+    {
+        if(number == 'C')
+        {
+            calcInput.value = '';
+        }
+    }
+    else
+    {
+        
+    const previousNumber = calcInput.value;
+    const newNumber = previousNumber + number;
+    calcInput.value = newNumber;
+
+    }
+    
+}
+
+);
+
+function verifyPin()
+{
+    const pin = document.getElementById('display-pin').value;
+    const typedNumbers = document.getElementById('typed-numbers').value;
+    const success = document.getElementById('notify-success');
+    const error = document.getElementById('notify-fail');
+    
+    if(typedNumbers == pin)
+    {
+        success.style.display = 'block';
+        error.style.display = 'none';
+    }
+    else
+    {        
+        error.style.display = 'block';
+        success.style.display = 'none';
+    }
+}
